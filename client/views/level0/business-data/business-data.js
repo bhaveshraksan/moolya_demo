96c6fbfc0b5fdfcdd836aca4411785aecbc64400
murali526 <muralidharreddy.kunduru@raksan.in>
2016-11-17 23:37:40 +0530

    isotope js file removed

--- client/views/level0/business-data/business-data.js
+++ client/views/level0/business-data/business-data.js
@@ -1,8 +1,8 @@
 Template.businessData.onRendered(function(){
-    $('.data-grid').isotope({
+  /*  $('.data-grid').isotope({
         itemSelector: '.grid-item',
         percentPosition: true,
         masonry: {
             columnWidth: '.grid-sizer'
-        }});
+        }});*/
 });
