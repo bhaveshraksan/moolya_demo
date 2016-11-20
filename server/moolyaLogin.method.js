fb917756e0b6d855bc08fb010f30947f43d3af74
MohasinTrochil <mohasin.m@trochil.com>
2016-11-20 12:16:39 +0530

    1) Company Name and Url fields added. 2)Unique constraint added to email field

--- server/moolyaLogin.method.js
+++ server/moolyaLogin.method.js
@@ -2,7 +2,11 @@
 Meteor.methods({
 	registerMoolyaUser: function (regObj) {
 		check(regObj,Object);
-		var id = MlRegistration.insert(regObj);
+		try{
+			var id = MlRegistration.insert(regObj);
+		}catch(e){
+			throw new Meteor.Error(403,"An User with this email already exists,Please register with a new email address");
+		}
 		MoolyaNotifier.moolyaUserRegistrationNotification(id);
 		MoolyaNotifier.moolyaUserDetailsForAdminNotification(id);
 		return "Success";
